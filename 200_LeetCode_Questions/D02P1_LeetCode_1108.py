341bda88b87cd2af9d44ac0f24b3b201df00780d
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-08-26 01:19:28 +0530

    Update D02P1_LeetCode_1108.py

--- 200_LeetCode_Questions/D02P1_LeetCode_1108.py
+++ 200_LeetCode_Questions/D02P1_LeetCode_1108.py
@@ -1,1 +1,14 @@
-# Python3 code coming soon
+#day2
+#question4
+#question link=https://leetcode.com/problems/defanging-an-ip-address/
+#for more such solutions go to:-    https://github.com/prakash079
+
+
+---------------------------------------
+
+#solution
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        address=address.replace('.',"[.]")
+        return address 
+      
