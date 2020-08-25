e1e548931f63c272f7a78e043f089352a5924548
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-08-26 01:14:41 +0530

    Update D01P2_LeetCode_1470.py

--- 200_LeetCode_Questions/D01P2_LeetCode_1470.py
+++ 200_LeetCode_Questions/D01P2_LeetCode_1470.py
@@ -1,1 +1,17 @@
-# Python3 code coming soon
+#question2
+#question link=https://leetcode.com/problems/shuffle-the-array/
+
+---------------------------
+#solution
+""" the simplest solution for this problem we can think of :
+is that we can take an empty array and merge/append
+each element of the array from index 0 and index n as there are
+n elements difference in the sorted arrays"""
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        l=[]
+        for i in range(0,n):
+            
+            l.append(nums[i])
+            l.append(nums[n+i])
+        return l
