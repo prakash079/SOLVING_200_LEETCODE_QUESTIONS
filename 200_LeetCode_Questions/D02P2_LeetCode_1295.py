401aa4ee26cb967bec3adbf09ce5e78bd5548492
Prakash Kumar Pathak <43916547+prakash079@users.noreply.github.com>
2020-08-26 01:22:03 +0530

    Update D02P2_LeetCode_1295.py

--- 200_LeetCode_Questions/D02P2_LeetCode_1295.py
+++ 200_LeetCode_Questions/D02P2_LeetCode_1295.py
@@ -1,1 +1,15 @@
-# Python3 code coming soon
+#question5
+#question link=https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+#for more such solutions go to:-https://github.com/prakash079
+
+-------------------------------------
+#solution
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count=0
+        for i in range(0,len(nums)):
+            nums[i]=str(nums[i])
+            n=len(nums[i])
+            if(n%2==0):
+                count+=1
+        return count
